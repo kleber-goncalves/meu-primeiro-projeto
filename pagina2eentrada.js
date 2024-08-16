d4d49eb1df215786aa49878a463ab5d94ae93527
Kleber Lucas.G.S. <lenovomicrosof1@gmail.com>
2024-08-16 14:28:14 -0300

    atualizei a projeto

---
+++ pagina2eentrada.js
@@ -0,0 +1,33 @@
+/*
+    var card = document.querySelector('.card');
+
+    window.onload = () => {
+        
+        car.onload = () => {
+                   // Aplica os estilos 
+            card.style.setProperty('--card-after-filter', 'blur(30px)');
+            card.style.setProperty('--card-before-transform', 'rotate(-90deg) scaleX(1.34) scaleY(0.77)');
+        
+        
+        }
+    }
+
+
+
+
+
+
+
+
+
+/*
+function entrando()
+{
+    if (entrando){
+            // bloco //
+        document.pg2.card.style.setProperty('--card-after-filter', 'blur(30px)');
+        document.pg2.card.style.setProperty('--card-before-transform', 'rotate(-90deg) scaleX(1.34) scaleY(0.77)');
+        
+    }
+}
+  */
